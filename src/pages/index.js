127d26bd2949dbe2605c7d19178ea139e67c14c3
jeorgedonato <jeorgekhenr@gmail.com>
2022-02-15 09:33:58 -0800

    Fixing home

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import * as React from "react"
 import '../styles/index.scss'
 import Layout from '../components/Layout'
 import { Router } from '@reach/router'
-// import Home from '../components/Home'
+import Home from '../components/Home'
 
 
-const Home = React.lazy(() => import('../components/Home'));
+// const Home = React.lazy(() => import('../components/Home'));
 const About = React.lazy(() => import('../components/About'));
 const Portfolio = React.lazy(() => import('../components/Projects'));
 const Skills = React.lazy(() => import('../components/Skills'));
@@ -22,7 +22,8 @@ const IndexPage = () => {
     <>
         <Layout>
           <Router>
-            <LazyComponent Component={Home} path="/" />
+            <Home path="/" />
+            {/* <LazyComponent Component={Home} path="/" /> */}
             <LazyComponent Component={About} path="about" />
             <LazyComponent Component={Portfolio} path="portfolio" />
             <LazyComponent Component={Skills} path="skills" />
