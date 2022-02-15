77ded8a12e698311b06ca05d3e21411881821dbe
jeorgedonato <jeorgekhenr@gmail.com>
2022-02-15 09:41:52 -0800

    select none

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
     return (
         <>
             <BgImage image={pluginImage} style={{ height: `calc(100vh - ${navHeight}px)`, width: `100%` }} >
-                <div className="z-10 relative w-full h-full flex flex-col justify-center items-center gap-y-6 font-poppins text-white">
+                <div className="z-10 relative w-full h-full flex flex-col justify-center items-center gap-y-6 font-poppins text-white select-none">
                     <h1 className="intro-text font-bold text-3xl md:text-5xl relative">Hi! I'm Jeorge Donato</h1>
                     <div className="animation-text font-bold text-xl md:text-3xl relative">
                         <span className="text-1st overflow-hidden inline-block relative">A Full Stack Web Developer</span>
